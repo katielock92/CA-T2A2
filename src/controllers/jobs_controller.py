a269d4389a3e53f07f76e63df37f67af9041b571
katielock92 <katielock92@gmail.com>
2023-07-17 13:50:30 +1000

    updated job schemas and routes

--- src/controllers/jobs_controller.py
+++ src/controllers/jobs_controller.py
@@ -1,10 +1,21 @@
 from main import db
-from models.jobs import Job, job_schema, jobs_schema
+from models.jobs import (
+    Job,
+    job_schema,
+    jobs_schema,
+    job_view_schema,
+    jobs_view_schema,
+    job_staff_view_schema,
+    jobs_staff_view_schema,
+)
 from models.users import User
 
 from flask import Blueprint, jsonify, request, abort
 from flask_jwt_extended import jwt_required, get_jwt_identity
 import functools
+from sqlalchemy.exc import IntegrityError
+from marshmallow import ValidationError
+from psycopg2 import errorcodes
 
 jobs = Blueprint("jobs", __name__, url_prefix="/jobs")
 
@@ -41,37 +52,66 @@ def authorise_as_staff(fn):
     return wrapper
 
 
-# lists all jobs using a GET request:
+# lists open jobs using a GET request:
 @jobs.route("/", methods=["GET"])
+def get_open_jobs():
+    jobs_list = Job.query.filter_by(status="Open")
+    admin = True  # add check on database here for user access
+    if admin:
+        result = jobs_staff_view_schema.dump(jobs_list)
+        return jsonify(result)
+    else:
+        result = jobs_view_schema.dump(jobs_list)
+        return jsonify(result)
+
+
+# lists all jobs (regardless of status) using a GET request:
+@jobs.route("/all/", methods=["GET"])
 def get_all_jobs():
     jobs_list = Job.query.all()
-    result = jobs_schema.dump(jobs_list)
-    return jsonify(result)
+    admin = False  # add check on database here
+    if admin:
+        result = jobs_staff_view_schema.dump(jobs_list)
+        return jsonify(result)
+    else:
+        result = jobs_view_schema.dump(jobs_list)
+        return jsonify(result)
 
 
-# lists all open jobs using a GET request:
-@jobs.route("/open/", methods=["GET"])
-def get_open_jobs():
-    pass
-# optional route for later if I have time
+# add conditions to return different schemas depending on user access
 
-# creates a new job using a POST request:
-# need to add JWT auth to this later
+
+# allows an authorised user to create a new job using a POST request:
 @jobs.route("/", methods=["POST"])
+@jwt_required()
+@authorise_as_staff
 def create_job():
-    job_fields = job_schema.load(request.json)
-    new_job = Job()
-    new_job.title = job_fields["title"]
-    new_job.description = job_fields["description"]
-    new_job.department = job_fields["department"]
-    new_job.location = job_fields["location"]
-    new_job.salary_budget = job_fields["salary_budget"]
-    # new_job.hiring_manager_id = job_fields["hiring_manager_id"]
-    # add a validation for the hiring manager ID to give a better error?
-    db.session.add(new_job)
-    db.session.commit()
-    return jsonify(job_schema.dump(new_job))
-
+    try:
+        job_fields = job_schema.load(request.json)
+        new_job = Job()
+        new_job.title = job_fields["title"]
+        new_job.description = job_fields["description"]
+        new_job.department = job_fields["department"]
+        new_job.location = job_fields["location"]
+        new_job.salary_budget = job_fields["salary_budget"]
+        new_job.hiring_manager_id = job_fields["hiring_manager_id"]
+        db.session.add(new_job)
+        db.session.commit()
+        return jsonify(job_staff_view_schema.dump(new_job)), 201
+    except ValidationError:
+        return {
+            "error": "A required field has not been provided, please try again."
+        }, 409
+    except IntegrityError as err:
+        if err.orig.pgcode == errorcodes.NOT_NULL_VIOLATION:
+            return {
+                "error": f"The '{err.orig.diag.column_name}' field is required, please try again."
+            }, 409
+        else:
+            return {
+                "error": "Invalid hiring manager id provided, please try again."
+            }, 409
+            
 
 # allows an authorised staff member to update a job using a PUT or POST request:
 @jobs.route("/<int:id>/", methods=["PUT", "POST"])
@@ -88,9 +128,11 @@ def update_job(id):
         job.location = body_data.get("location") or job.location
         job.salary_budget = body_data.get("salary_budget") or job.salary_budget
         job.status = body_data.get("status") or job.status
-        # add line to update hiring manager id once that's working in add
+        job.hiring_manager_id = (
+            body_data.get("hiring_manager_id") or job.hiring_manager_id
+        )
         db.session.commit()
-        return job_schema.dump(job)
+        return job_staff_view_schema.dump(job)
     else:
         return {"error": f"Job not found with id {id}"}, 404
 
--- src/models/jobs.py
+++ src/models/jobs.py
@@ -18,7 +18,7 @@ class Job(db.Model):
     salary_budget = db.Column(
         db.Integer(), nullable=False
     )  # need to find SQL type for money if possible
-    hiring_manager_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    hiring_manager_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
 
     # adding child relationship with Users for HM users, does not cascade delete:
     hiring_manager = db.relationship("User", back_populates="jobs")
@@ -29,25 +29,72 @@ class Job(db.Model):
 
 # creating a Schema with Marshmallow to allow us to serialise Jobs into JSON:
 class JobSchema(ma.Schema):
-
-    # nested schemas:
-    hiring_manager = fields.Nested(
-        "UserSchema", only=["first_name", "last_name", "email"]
-    )
-
     # field validations:
     title = fields.String(
         required=True,
         validate=And(
             Length(min=4, error="Job title must be at least 4 characters long"),
+            Length(
+                max=50, error="Job title can only be a maximum of 50 characters long"
+            ),
             Regexp(
                 "^[a-zA-Z0-9() -]+",
                 error="Title must contain only letters, numbers, spaces and certain special characters - please try again.",
             ),
         ),
     )
+    department = fields.String(
+        required=True,
+        validate=And(
+            Length(min=2, error="Department must be at least 2 characters long"),
+            Regexp(
+                "^[a-zA-Z0-9() -]+",
+                error="Department name must contain only letters, numbers, spaces and certain special characters - please try again.",
+            ),
+        ),
+    )
+    location = fields.String(
+        required=True,
+        validate=And(
+            Length(min=2, error="Location must be at least 2 characters long"),
+            Regexp(
+                "^[a-zA-Z0-9() -]+",
+                error="Location name must contain only letters, numbers, spaces and certain special characters - please try again.",
+            ),
+        ),
+    )
+    description = fields.String(required=True)
+    salary_budget = fields.Integer(required=True)
 
+    hiring_manager_id = fields.Integer(required=True)
     status = fields.String(validate=OneOf(VALID_STATUSES))
+
+    class Meta:
+        fields = (
+            "id",
+            "title",
+            "department",
+            "location",
+            "description",
+            "hiring_manager_id",
+            "status",
+            "salary_budget",
+        )
+        ordered = True
+
+
+# single job schema, when one job needs to be retrieved
+job_schema = JobSchema()
+# multiple job schema, when many jobs need to be retrieved
+jobs_schema = JobSchema(many=True)
+
+
+# additional Schema for displaying jobs to authenticated staff:
+class JobStaffViewSchema(ma.Schema):
+    # nested schemas:
+    hiring_manager = fields.Nested(
+        "UserSchema", only=["first_name", "last_name", "email"]
+    )
 
     class Meta:
         fields = (
@@ -64,6 +111,19 @@ class JobSchema(ma.Schema):
 
 
 # single job schema, when one job needs to be retrieved
-job_schema = JobSchema()
+job_staff_view_schema = JobStaffViewSchema()
 # multiple job schema, when many jobs need to be retrieved
-jobs_schema = JobSchema(many=True)
+jobs_staff_view_schema = JobStaffViewSchema(many=True)
+
+
+# additional Schema for displaying jobs to non-authenticated users:
+class JobViewSchema(ma.Schema):
+    class Meta:
+        fields = ("title", "department", "location", "description", "status")
+        ordered = True
+
+
+# single job schema, when one job needs to be retrieved
+job_view_schema = JobViewSchema()
+# multiple job schema, when many jobs need to be retrieved
+jobs_view_schema = JobViewSchema(many=True)
